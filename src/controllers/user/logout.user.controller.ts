2bed558686c0e831e211156432c7b2ffe31486a9
growTecsVedang <growtecs.vedang@gmail.com>
2023-06-16 23:12:36 +0530

    update: logout function

--- src/controllers/user/logout.user.controller.ts
+++ src/controllers/user/logout.user.controller.ts
@@ -20,7 +20,6 @@ export const handleLogout = async (
     httpOnly: true,
     secure: false,
     sameSite: "lax",
-    domain: ".app",
     path: "/",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
