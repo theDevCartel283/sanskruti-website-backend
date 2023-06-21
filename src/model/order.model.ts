de46f9084e92ae1a04f755386d57741eced2c1e0
growTecsVedang <growtecs.vedang@gmail.com>
2023-06-21 18:23:17 +0530

    create: order model & order controllers

--- src/model/order.model.ts
+++ src/model/order.model.ts
@@ -1,6 +1,71 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
+  userInfo: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  orderItems: [
+    {
+      id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      varient: {},
+      discount: {
+        type: Number,
+        required: true,
+      },
+      gst: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  orderInfo: {
+    Date: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    SubTotal: {
+      type: Number,
+      required: true,
+    },
+    ShippingCost: {
+      type: Number,
+      required: true,
+    },
+    Totaldiscount: {
+      type: Number,
+      required: true,
+    },
+    TotalGST: {
+      type: Number,
+      required: true,
+    },
+    Amount: {
+      type: Number,
+      required: true,
+    },
+  },
   shippingInfo: {
     id: {
       type: String,
@@ -10,6 +75,10 @@ const orderSchema = new mongoose.Schema({
       type: String,
       required: true,
       default: null,
+    },
+    email: {
+      type: String,
+      required: true,
     },
     contactNo: {
       type: Number,
@@ -58,6 +127,7 @@ const orderSchema = new mongoose.Schema({
     currency: {
       type: String,
       required: true,
+      default: "INR",
     },
     timestamp: {
       type: String,
@@ -70,6 +140,51 @@ const orderSchema = new mongoose.Schema({
     referenceId: {
       type: String,
       required: true,
+    },
+  },
+  cancellationInfo: {
+    returnRequest: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isCancelled: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    Amount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  returnInfo: {
+    cancelRequest: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    isReturned: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    Amount: {
+      type: Number,
+      required: true,
+      default: 0,
     },
   },
 });
