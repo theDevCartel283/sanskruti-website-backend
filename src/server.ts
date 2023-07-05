84fbfda6108fa2670c007b68eb02d8f9b2b578e2
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-05 16:43:31 +0530

    ⬆️update: cors allowed

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ const PORT = Number(env.PORT) || 4000;
 // app.use(cors(corsOptions));
 app.use(
   cors({
-    origin: "*", // Replace with your allowed origin or use '*' to allow all origins
+    origin: allowedOrigins, // Replace with your allowed origin or use '*' to allow all origins
     credentials: true,
     allowedHeaders: "Content-Type,Authorization", // Specify the allowed headers for requests
     methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
