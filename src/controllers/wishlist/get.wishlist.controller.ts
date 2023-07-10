333d4e43202214c5da923df75b2c2a8eba58969d
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-10 19:51:57 +0530

    ➕Add: try catch to wishlist

--- src/controllers/wishlist/get.wishlist.controller.ts
+++ src/controllers/wishlist/get.wishlist.controller.ts
@@ -2,30 +2,39 @@ import { Request, Response } from "express";
 import { TokenPayload } from "../../utils/jwt.utils";
 import wishlistModel from "../../model/wishlist.model";
 import getProductsFromIds from "../../utils/getProductsFromIds";
+import logger from "../../utils/logger.utils";
 
 const handleGetWishlist = async (
   req: Request<{}, {}, TokenPayload>,
   res: Response
 ) => {
-  const userWishlist = await wishlistModel.findOne({
-    userId: req.body.userUniqueIdentity,
-  });
+  try {
+    const userWishlist = await wishlistModel.findOne({
+      userId: req.body.userUniqueIdentity,
+    });
 
-  if (!userWishlist) return res.send({ list: [] });
+    if (!userWishlist) return res.send({ list: [] });
 
-  const { filteredArray, emptyArray } = await getProductsFromIds(
-    userWishlist.products
-  );
-  if (emptyArray && emptyArray.length !== 0) {
-    userWishlist.products = userWishlist.products.filter(
-      (product) => !emptyArray.includes(product._id)
+    const { filteredArray, emptyArray } = await getProductsFromIds(
+      userWishlist.products
     );
-    await userWishlist.save();
+    if (emptyArray && emptyArray.length !== 0) {
+      userWishlist.products = userWishlist.products.filter(
+        (product) => !emptyArray.includes(product._id)
+      );
+      await userWishlist.save();
+    }
+    res.status(200).send({
+      ids: userWishlist.products,
+      list: filteredArray || [],
+    });
+  } catch (err) {
+    logger.error("get wishlist error " + err);
+    res.status(500).send({
+      ids: [],
+      list: [],
+    });
   }
-  res.status(200).send({
-    ids: userWishlist.products,
-    list: filteredArray || [],
-  });
 };
 
 export default handleGetWishlist;
