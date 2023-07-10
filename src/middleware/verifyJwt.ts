9ebbacf37f4004d95c17df93b30f32a0b953d459
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-10 19:54:30 +0530

    ➕Add: try catch to verify jwt middleware

--- src/middleware/verifyJwt.ts
+++ src/middleware/verifyJwt.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyJwt } from "../utils/jwt.utils";
+import logger from "../utils/logger.utils";
 
 export type VerifyRequest<TParams, TBody, TQuery> = Request<
   TParams,
@@ -13,30 +14,35 @@ const verifyAccessJwt = async (
   res: Response,
   next: NextFunction
 ) => {
-  const cookie = req.cookies;
-  if (!cookie?.accessToken)
-    return res
-      .status(401)
-      .json({ message: "something went wrong", type: "info" });
-  const accessToken: string = cookie.accessToken;
+  try {
+    const cookie = req.cookies;
+    if (!cookie?.accessToken)
+      return res
+        .status(401)
+        .json({ message: "something went wrong", type: "info" });
+    const accessToken: string = cookie.accessToken;
 
-  const { valid, decoded, expired } = verifyJwt(
-    accessToken,
-    "ACCESS_TOKEN_PUBLIC"
-  );
+    const { valid, decoded, expired } = verifyJwt(
+      accessToken,
+      "ACCESS_TOKEN_PUBLIC"
+    );
 
-  if (valid && decoded && !expired) {
-    //   const isBanned = await checkBanned(decoded.userUniqueIdentity);
-    //   if (isBanned)
-    //     return res.status(401).send({ message: 'Unauthorized', type: 'warning' });
+    if (valid && decoded && !expired) {
+      //   const isBanned = await checkBanned(decoded.userUniqueIdentity);
+      //   if (isBanned)
+      //     return res.status(401).send({ message: 'Unauthorized', type: 'warning' });
 
-    req.body.userUniqueIdentity = decoded.userUniqueIdentity;
-    req.body.provider = decoded.provider;
-    req.body.userRole = decoded.userRole;
-    return next();
+      req.body.userUniqueIdentity = decoded.userUniqueIdentity;
+      req.body.provider = decoded.provider;
+      req.body.userRole = decoded.userRole;
+      return next();
+    }
+
+    return res.status(401).send({ message: "Unauthorized", type: "warning" });
+  } catch (err) {
+    logger.error("verify jwt error " + err);
+    return res.status(401).send({ message: "Unauthorized", type: "warning" });
   }
-
-  return res.status(401).send({ message: "Unauthorized", type: "warning" });
 };
 
 export default verifyAccessJwt;
