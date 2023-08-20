cec93f9221f740709acbb10008cfc10ef6f16672
growTecsVedang <growtecs.vedang@gmail.com>
2023-08-20 12:53:57 +0530

    update : category api updated

--- src/controllers/category/deleteCategoryImage.controller.ts
+++ src/controllers/category/deleteCategoryImage.controller.ts
@@ -1,23 +1,34 @@
 import { Request, Response } from "express";
+import axios from "axios";
 import fs from "fs";
 import categoryModel from "../../model/category.model";
 
 const deleteCategoryImage = async (req: Request, res: Response) => {
-  const imagePath = req.body.imagePath;
   const categoryAlreadyExists = await categoryModel.findOne({
     _id: req.query._id,
   });
+  console.log(req.query);
+  const url_params = req.query;
+  const response = await axios.delete(
+    `${process.env.CDN_ENDPOINT}/cdn/v1/images/deleteImage?name=${url_params.name}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const data = response.data;
 
   if (categoryAlreadyExists) {
     categoryAlreadyExists.Image = "";
     const category = await categoryAlreadyExists.save({
       validateBeforeSave: false,
     });
-    // fs.unlinkSync(imagePath);
     res.status(200).json({
       type: "success",
       message: "category image deleted successfully",
-      category,
+      // category,
     });
   } else {
     res.status(500).json({
