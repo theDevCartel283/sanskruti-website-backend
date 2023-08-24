dbc51ebbbb25b2a5800767cccbeafd8b73714f2d
Vishal-Kamath <vishalkamath853@gmail.com>
2023-08-24 23:15:38 +0530

    ➕Add: get payment status

---
+++ src/controllers/order/getPaymentStatus.order.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import logger from "../../utils/logger.utils";
+import PaymentModel from "../../model/payment.model";
+
+const handleGetPaymentStatus = async (
+  req: Request<{ id: string }>,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+    const payment = await PaymentModel.findOne({ orderId: id });
+
+    if (!payment)
+      return res
+        .status(404)
+        .send({ message: "Order not found", type: "warning" });
+
+    res.status(200).send({
+      orderId: payment.orderId,
+      status: payment.paymentInfo.order_status,
+      amount: payment.paymentInfo.amount,
+    });
+  } catch (err) {
+    logger.error("get status error " + err);
+    return res.status(500).send({
+      message: "something went wrong",
+      type: "info",
+    });
+  }
+};
+
+export default handleGetPaymentStatus;
--- src/controllers/order/index.order.controller.ts
+++ src/controllers/order/index.order.controller.ts
@@ -6,6 +6,7 @@ import getOrder from "./getOrders.controller";
 import GetOrderDetails from "./getOrderDetails.controller";
 import handleCancellationRequest from "./requestCancel.order.contoller";
 import handleUpdateOrderFromAdmin from "./update.order.controller";
+import handleGetPaymentStatus from "./getPaymentStatus.order.controller";
 
 export {
   handlePlaceOrder,
@@ -15,4 +16,5 @@ export {
   GetOrderDetails,
   handleCancellationRequest,
   handleUpdateOrderFromAdmin,
+  handleGetPaymentStatus,
 };
